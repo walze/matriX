b2f883fcb563b769f23fbb7ccdd097049194b017
wallace fares <wallacefares@gmail.com>
2022-10-31 19:46:24 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log(123);
+const allChars = Array.from({ length: 2 ** 16 }, (_, i) => i + 1).map((i) =>
+  String.fromCharCode(i)
+);
+
+console.log(allChars);
+
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+if (!ctx) throw "something is wrong with you";
